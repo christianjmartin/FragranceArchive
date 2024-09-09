b252caf08bd10d5ba665f67460d6ead0adaba216
cjmartin232324 <cmart207@uic.edu>
2024-09-09 17:44:05 -0500

    add keyerror handler back

--- main.py
+++ main.py
@@ -948,10 +948,10 @@ def logout():
 
 
 
-# @app.errorhandler(KeyError)
-# def handle_key_error(e):
-#     # Handle the error when a session key is missing
-#     return redirect(url_for('home'))
+@app.errorhandler(KeyError)
+def handle_key_error(e):
+    # Handle the error when a session key is missing
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
